707941c6fbc26d1a4dc6be079210a62b40b1d3bb
Branson Boggia <bmanboggia@yahoo.com>
2018-11-26 13:09:49 -0500

    Initial commit

---
+++ DoodleJumpClone/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  DoodleJumpClone
+//
+//  Created by Branson Boggia on 11/26/18.
+//  Copyright © 2018 Pinetree Development Studios. All rights reserved.
+//
+
+import UIKit
+
+class game: NSObject {
+    
+}
---
+++ DoodleJumpClone/Sprite.swift
@@ -0,0 +1,13 @@
+//
+//  Sprite.swift
+//  DoodleJumpClone
+//
+//  Created by Branson Boggia on 11/26/18.
+//  Copyright © 2018 Pinetree Development Studios. All rights reserved.
+//
+
+import UIKit
+
+class sprite: NSObject {
+    
+}
